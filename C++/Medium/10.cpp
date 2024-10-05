5354b525d4719827410eef0dcc47f08d5a2da482
ShreyasAndurlekar <and.shreyas@gmail.com>
2024-10-05 12:55:20 +0530

    Solved 10 boss

--- C++/Medium/10.cpp
+++ C++/Medium/10.cpp
@@ -7,10 +7,15 @@ int main()
     {
     case 1:
         cout << "One" << endl;
+	break;
     case 2:
         cout << "Two" << endl;
+	break;
     case 3:
         cout << "Three" << endl;
+	break;
+    default:
+	cout << "Invalid";
     }
     return 0;
 }
