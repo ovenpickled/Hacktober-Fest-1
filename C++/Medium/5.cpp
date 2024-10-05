517eb00f7d2a278f76d8e0dcb0151f6ac43b5037
ShreyasAndurlekar <and.shreyas@gmail.com>
2024-10-05 12:28:25 +0530

    pachbhai

--- C++/Medium/5.cpp
+++ C++/Medium/5.cpp
@@ -6,14 +6,17 @@ int main()
     char str[100];
     int count = 0;
     cout << "Enter a string: ";
-    cin >> str;
+    cin.getline(str, 100);
     for (int i = 0; i <= strlen(str); i++)
     {
-        if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u')
+        if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u' ||
+         str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U')
         {
             count++;
         }
     }
-    cout << "Number of vowels: " << count - 1 << endl;
+    cout << "Number of vowels: " << count << endl;
+
+    // count - 1 ko hata diya kyuki obv we want accurate count and cin can only accept one string not mulitple have to use getline method and added capital letters
     return 0;
 }
