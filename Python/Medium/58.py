f9e64361a39314024004ed20d2fc76036e0262d3
san-coded <109622291+ovenpickled@users.noreply.github.com>
2024-10-05 16:20:50 +0530

    Update 58.py

--- Python/Medium/58.py
+++ Python/Medium/58.py
@@ -1,11 +1,13 @@
- def access_nested_list(nested_list, outer_index, inner_index):
+def access_nested_list(nested_list, outer_index, inner_index):
  try:
- return nested_list[outer_index][inner_index]
+    return nested_list[outer_index][inner_index]
  except IndexError as e:
- print(f"IndexError: {e}")
- def main():
+    print(f"IndexError: {e}")
+ 
+def main():
  nested_list = [[1, 2, 3], [4, 5], [6]]
- result = access_nested_list(nested_list, 1, 5)
+ result = access_nested_list(nested_list, 1, 1)
  if result is not None:
- print(f"Accessed value: {result}")
- main()
+    print(f"Accessed value: {result}")
+ 
+main()
