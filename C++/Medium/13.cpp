885ed6da343a168c128f9ae696e29ca20f644d76
ShreyasAndurlekar <and.shreyas@gmail.com>
2024-10-05 13:13:04 +0530

    13branchsubmit

--- C++/Medium/13.cpp
+++ C++/Medium/13.cpp
@@ -3,10 +3,13 @@ using namespace std;
 int main()
 {
     int x = 5;
-    if (x > 3)
+    if (x > 3){ // nested if else conditions were not there
+
         if (x < 10)
             cout << "x is between 3 and 10" << endl;
         else
             cout << "x is greater than 10" << endl;
+    }
+        
     return 0;
 }
