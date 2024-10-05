ddefa73a6906a3aca8ecc59ab17bdae3978f3b72
ShreyasAndurlekar <and.shreyas@gmail.com>
2024-10-05 12:40:57 +0530

    8MEDIUMLESGO

--- C++/Medium/8.cpp
+++ C++/Medium/8.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
-str Point
-{
-    int x, y;
-    int main()
+struct Point{
+
+    int x;
+    int y;
+};
+
+// C++ me bhai classes hai this is C stuff but basically it's like your own data type
+
+int main()
     {
-        Point x = {10, 20};
+        Point p = {10, 20};
         std::cout << "x: " << p.x << ", y: " << p.y;
         return 0;
     }
-}
+
