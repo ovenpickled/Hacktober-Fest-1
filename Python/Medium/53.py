12a2fccd2928ba74b7c385d64485c6dda3740511
san-coded <109622291+ovenpickled@users.noreply.github.com>
2024-10-05 16:12:58 +0530

    Update 53.py

--- Python/Medium/53.py
+++ Python/Medium/53.py
@@ -1,13 +1,15 @@
- def calculate_average(lst):
+def calculate_average(lst):
  total = 0
  for num in lst:
- total += num
+    total += num
  return total / len(lst)
- def main():
- numbers = []
+ 
+def main():
+ numbers = [1,2,3]
  try:
- avg = calculate_average(numbers)
- print(f"Average: {avg}")
-except ZeroDivisionError as e:
- print(f"ZeroDivisionError: {e}")
- main()
+    avg = calculate_average(numbers)
+    print(f"Average: {avg}")
+ except ZeroDivisionError as e:
+    print(f"ZeroDivisionError: {e}")
+ 
+main()
