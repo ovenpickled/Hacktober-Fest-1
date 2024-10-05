5e9bb7648753d7867b5ca901c69d2d36d413e1cd
ayushyadav092005 <ayushyadav092005@gmail.com>
2024-10-05 12:21:10 +0530

    Update 5.java

--- Java/Easy/5.java
+++ Java/Easy/5.java
@@ -1,6 +1,6 @@
 class Main {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3};
-        System.out.println(arr);
+        System.out.println(arr[0],arr[1],arr[2]);
     }
 }
