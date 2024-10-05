137141058d69934000f31af6f1580e85dee20e6a
san-coded <109622291+ovenpickled@users.noreply.github.com>
2024-10-05 16:17:07 +0530

    Update 56.py

--- Python/Medium/56.py
+++ Python/Medium/56.py
@@ -1,12 +1,14 @@
 def sort_list(lst):
  try:
- lst.sort()
-return lst
+    lst.sort()
+    return lst
  except TypeError as e:
- print(f"TypeError: {e}")
- def main():
- items = [5, "banana", 3, "apple"]
+    print(f"TypeError: {e}")
+ 
+def main():
+ items = ["5", "banana", "3", "apple"]
  sorted_items = sort_list(items)
  if sorted_items:
- print(f"Sorted items: {sorted_items}")
- main()
+    print(f"Sorted items: {sorted_items}")
+ 
+main()
