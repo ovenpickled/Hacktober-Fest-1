c39542572ff3657b2b7b7a9eeac24e5d518adb90
ayushyadav092005 <ayushyadav092005@gmail.com>
2024-10-05 12:13:04 +0530

    Update 2.java

--- Java/Easy/2.java
+++ Java/Easy/2.java
@@ -1,6 +1,8 @@
-class Main {
-    public static void main(String[] args) {
+class Main
+{
+    public static void main(String[] args)
+    {
         int[] numbers = {1, 2, 3};
-        System.out.println(numbers(0));
+        System.out.println(numbers[0]);
     }
 }
