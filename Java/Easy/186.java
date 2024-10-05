3009715cbf0684b2d01198d2b72c53d030aef07f
Ruchik Panwal <ruchik.panwal@gmail.com>
2024-10-05 13:42:26 +0530

    Update 186.java

--- Java/Easy/186.java
+++ Java/Easy/186.java
@@ -2,6 +2,6 @@ class Main {
     public static void main(String[] args) {
         int a = 5;
         int b = 10;
-        System.out.println(a = b ? "Equal" : "Not Equal");
+        System.out.println(a == b ? "Equal" : "Not Equal");
     }
 }
