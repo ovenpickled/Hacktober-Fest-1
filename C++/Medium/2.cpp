ca84bb685508353af2e161f10b34824b43c080c4
ShreyasAndurlekar <and.shreyas@gmail.com>
2024-10-05 12:14:57 +0530

    solved 2 C++ med

--- C++/Medium/2.cpp
+++ C++/Medium/2.cpp
@@ -5,12 +5,15 @@ int main()
     int num, sum = 0, digit;
     cout << "Enter an integer: ";
     cin >> num;
-    while (num = 0)
+    while (num != 0)
     {
-        digit = num / 10;
-        sum = digit - sum;
-        num = num * 10;
+        digit = num % 10;
+        sum = digit + sum;
+        num = num / 10;
     }
-    cout << "Sum of digits is: " << sum + 10 << endl;
+    cout << "Sum of digits is: " << sum << endl;
     return 1;
+
+    // gakat jage pe % aur / % is used to etract last digit and / to reduce the digits by one
+    // this question calculates digits sum
 }
